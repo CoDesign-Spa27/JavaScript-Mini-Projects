1b7e6464418b0ddad968d349c368e8ca7722be53
2sandeep7 <ssandeep8696@gmail.com>
2023-10-29 12:48:17 -0500

    JS MINI PROJECTS

---
+++ MouseFollower/script.js
@@ -0,0 +1,15 @@
+const cursor=document.querySelector(".cursor");
+//follow cursor on mouse move
+document.addEventListener("mousemove",(e)=>{
+    let x=e.pageX;
+    let y=e.pageY;
+
+    cursor.style.top=y+"px";
+    cursor.style.left=x+"px";
+    cursor.style.display="block";
+})
+//cursor effects on mouseout
+document.addEventListener("mouseout",()=>{
+    cursor.style.display="none"
+}) 
+ 
---
+++ Stone,Paper&Sicssor-Game/script.js
@@ -0,0 +1,3 @@
+//Stone Paper Scissor
+let userInput=prompt("enter the Input ");
+let 
